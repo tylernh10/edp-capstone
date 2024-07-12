56ac0408c6cb07025f4739f16199b602f5c2b89a
noor-majid <noormajid101@gmail.com>
2024-07-12 15:27:03 -0400

    screens set up working on login

---
+++ react/src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+    return (
+        <div>
+            Home Page
+        </div>
+    )
+}
+
+export default Home;
