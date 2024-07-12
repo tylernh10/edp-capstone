cf57ce79347e1a813c1276a9c741c030887abe30
Tyler Hinrichs <thinrich@travelers.com>
2024-07-12 13:48:45 -0400

    set up script

---
+++ data/data-gen.py
@@ -0,0 +1,17 @@
+# Generating JSON data to be uploaded to MongoDB
+import names
+
+NUM_EMPLOYEES = 1000
+
+job_roles = []
+work_locations = []
+
+def generate_users():
+    pass
+
+def generate_employees():
+    pass
+
+if __name__ == "__main__":
+    generate_users()
+    generate_employees()
