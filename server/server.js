20e580d1b73faff82af4d451912d4dcb50aa89ac
Noor Majid <noor@MacBook-Pro-2.home>
2024-07-14 00:38:08 -0400

    login and employee uid endpoints added to server

--- server/server.js
+++ server/server.js
@@ -43,6 +43,55 @@ app.get('/enterprise/users', async (req, res) => {
     }
 });
 
+app.post('/enterprise/login', async (req, res) => {
+    try {
+        const userData = req.body;
+        console.log('User Data: ', userData);
+        const username = userData.username;
+        const password = userData.password;
+        console.log("username: ", username);
+        console.log("password: ", password);
+
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const users_collection = db.collection(usersCollection);
+        const user = await users_collection.findOne({ "username": username, "password": password });
+        if (user) {
+            console.log(user);
+            console.log(user.user_id);
+            res.status(200).send(user.user_id);
+        } else {
+            res.status(401).send("Invalid login credentials.");
+        }
+    } catch (err) {
+        console.error("Error: ", err);
+        res.status(500).send("Something went wrong - login.");
+    }
+});
+
+// POST employee info - specific user_id
+// note two ways to do this: url param or req body
+app.post('/enterprise/employee', async (req, res) => {
+// app.post('/enterprise/employee/:uid', async (req, res) => {
+    try {
+        // const userId = req.params.uid;
+        const userId = req.body.uid
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const emp_collection = db.collection(employeesCollection);
+        const employee = await emp_collection.findOne({ "user_id": userId });
+        if (employee) {
+            console.log(employee);
+            res.status(200).send(employee);
+        } else {
+            res.status(401).send("Employee not found.");
+        }
+    } catch (err) {
+        console.error("Error: ", err);
+        res.status(500).send("Something went wrong - employee user_id.");
+    }
+});
+
 app.listen(PORT, () => {
     console.log(`Server is listening on http://localhost:${PORT}`);
 })
