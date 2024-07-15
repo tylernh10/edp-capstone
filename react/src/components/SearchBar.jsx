22ed1fc9607bdd57c6597fdb3b55697c6b1f21d2
Noor Majid <noor@MacBook-Pro-2.home>
2024-07-14 20:17:31 -0400

    updated search filter in server; prototype search ui; search results but needs ui

--- react/src/components/SearchBar.jsx
+++ react/src/components/SearchBar.jsx
@@ -1,27 +1,75 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Select from 'react-select';
+import './Search.css';
 
-const SearchBar = (props) => {
+const jobRolesOptions = [
+    { value: 'Chief Executive Officer', label: 'Chief Executive Officer' },
+    { value: 'Chief Technology Officer', label: 'Chief Technology Officer' },
+    { value: 'Director of Engineering', label: 'Director of Engineering' },
+    { value: 'Engineering Manager', label: 'Engineering Manager' },
+    { value: 'Project Manager', label: 'Project Manager' },
+    { value: 'Technical Lead', label: 'Technical Lead' },
+    { value: 'Senior Software Engineer', label: 'Senior Software Engineer' },
+    { value: 'Software Engineer', label: 'Software Engineer' },
+    { value: 'Junior Developer', label: 'Junior Developer' },
+    { value: 'Intern', label: 'Intern' }
+];
+
+const workLocationsOptions = [
+    { value: 'New York City', label: 'New York City' },
+    { value: 'San Francisco', label: 'San Francisco' },
+    { value: 'San Jose', label: 'San Jose' },
+    { value: 'Los Angeles', label: 'Los Angeles' },
+    { value: 'San Diego', label: 'San Diego' },
+    { value: 'Chicago', label: 'Chicago' },
+    { value: 'Philadelphia', label: 'Philadelphia' },
+    { value: 'Dallas', label: 'Dallas' },
+    { value: 'Houston', label: 'Houston' },
+    { value: 'Phoenix', label: 'Phoenix' },
+    { value: 'Hartford', label: 'Hartford' }
+];
+
+const SearchBar = () => {
     const [searchTerm, setSearchTerm] = useState('');
+    const [selectedJobRoles, setSelectedJobRoles] = useState([]);
+    const [selectedWorkLocations, setSelectedWorkLocations] = useState([]);
+    const navigate = useNavigate();
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        fetch(`${import.meta.env.VITE_SOCKS_API_URL}/search`, {
-            method: "POST",
-            body: JSON.stringify({ searchTerm }),
-            headers: {
-                "Content-Type": "application/json",
-            },
-        })
-            .then((response) => response.json())
-            .then((data) => {
-                // Handle the response data
-                props.setData(data);
-                console.log(data);
-            })
-            .catch((error) => {
-                // Handle any errors
-                console.error(error);
+        
+        const query = new URLSearchParams();
+        
+        if (searchTerm) {
+            query.append('name', searchTerm);
+        }
+        
+        if (selectedJobRoles.length > 0) {
+            selectedJobRoles.forEach(role => query.append('role', role.value));
+        }
+        
+        if (selectedWorkLocations.length > 0) {
+            selectedWorkLocations.forEach(location => query.append('location', location.value));
+        }
+
+        console.log(`http://localhost:3000/enterprise/employees?${query.toString()}`);
+
+        try {
+            const response = await fetch(`http://localhost:3000/enterprise/employees?${query.toString()}`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
             });
+            if (response.ok) {
+                const employee = await response.json();
+                console.log(employee);
+                navigate(`/search?${query.toString()}`, { state: { searchData: employee } });
+            }
+        } catch (error) {
+            console.error(error);
+        }
     };
 
     const handleChange = (e) => {
@@ -30,12 +78,41 @@ const SearchBar = (props) => {
     };
 
     return (
-        <form className="d-flex" role="search" onSubmit={{}}>
-            <input className="form-control me-2" type="search"
-                placeholder="Search" aria-label="Search"
-                value={searchTerm} onChange={handleChange} />
-            <button className="btn btn-outline-success" type="submit">Search</button>
-        </form>
+        <div>
+            <form className="d-flex" role="search" onSubmit={handleSubmit}>
+                <input
+                    className="custom-search-bar"
+                    type="search"
+                    placeholder="Search"
+                    aria-label="Search"
+                    value={searchTerm}
+                    onChange={handleChange}
+                />
+                <div className="filters">
+                    <Select
+                        isMulti
+                        name="jobRoles"
+                        options={jobRolesOptions}
+                        className="basic-multi-select"
+                        classNamePrefix="select"
+                        placeholder="Select Job Roles"
+                        value={selectedJobRoles}
+                        onChange={setSelectedJobRoles}
+                    />
+                    <Select
+                        isMulti
+                        name="workLocations"
+                        options={workLocationsOptions}
+                        className="basic-multi-select"
+                        classNamePrefix="select"
+                        placeholder="Select Work Locations"
+                        value={selectedWorkLocations}
+                        onChange={setSelectedWorkLocations}
+                    />
+                </div>
+                <button className="btn btn-outline-success" type="submit">Search</button>
+            </form>
+        </div>
     );
 };
 
--- react/src/components/SearchResults.jsx
+++ react/src/components/SearchResults.jsx
@@ -1,13 +1,30 @@
 import React from "react";
-import { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+import './Search.css';
 
 const SearchResults = () => {
+    const location = useLocation();
+    const { searchData } = location.state || { searchData: [] };
+    console.log(searchData, "results")
+
     return (
         <div>
-            Search Results Page
+            <h1>Search Results</h1>
+            <div className="results-container">
+                {searchData.length > 0 ? (
+                    searchData.map((employee, index) => (
+                        <div key={index} className="employee-card">
+                            <h2>{employee.full_name}</h2>
+                            <p>Role: {employee.job_role}</p>
+                            <p>Location: {employee.work_location}</p>
+                        </div>
+                    ))
+                ) : (
+                    <p>No results found</p>
+                )}
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default SearchResults;
