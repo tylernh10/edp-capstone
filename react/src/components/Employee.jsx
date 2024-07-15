f46bcffd260e01289e38fc02cf1d585edba50e17
Tyler Hinrichs <thinrich@travelers.com>
2024-07-15 14:53:15 -0400

    start employee page

--- react/src/components/Employee.jsx
+++ react/src/components/Employee.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import City from "./City";
+import './Search.css';
 
 import Navigation from "./Navigation";
 
@@ -8,7 +10,22 @@ const Employee = (props) => {
     return (
         <div className="home-container">
             <Navigation />
-            Employee Page
+            <h2>Tyler Hinrichs <span>(you)</span></h2>
+            <div className="employee-card">
+                <span>Organization Info</span>
+                <div>Role</div>
+                <div>Manager</div>
+                <div>Salary</div>
+            </div>
+            <div>
+                Contact Info
+            </div>
+            <div>
+                Direct Reports
+            </div>
+            <div>
+                <City />
+            </div>
         </div>
     )
 }
